dfe5e9fb26f2eb3c58f8a8fea13421b8c3718289
Adam Sobecki <adasiek.96@outlook.com>
2016-12-10 22:56:05 +0100

    First sensors controller

---
+++ WebAPI/WebAPI/Controllers/SensorsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class SensorsController : ApiController
+    {
+        private double temperature;
+        private int sound;
+        private int light;
+
+        public void PostTemperature(double value)
+        {
+            temperature = value;
+        }
+
+        public void PostSound(int value)
+        {
+            sound = value;
+        }
+
+        public void PostLight(int value)
+        {
+            light = value;
+        }
+
+        public IHttpActionResult IsOk()
+        {
+            return Ok(temperature > 20 && sound < 200 && light < 200);
+        }
+    }
+}
