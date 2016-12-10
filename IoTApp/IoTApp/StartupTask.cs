bb94dcfdd50a13104030c8b663975d473adea5a3
Adam Sobecki <adasiek.96@outlook.com>
2016-12-10 19:44:26 +0100

    UWP instead of Console App

---
+++ IoTApp/IoTApp/StartupTask.cs
@@ -0,0 +1,20 @@
+﻿using GrovePi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace IoTApp
+{
+    class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            DeviceFactory.Build.Led(Pin.DigitalPin2).ChangeState(GrovePi.Sensors.SensorStatus.On);
+            Task.Delay(3000).Wait();
+            DeviceFactory.Build.Led(Pin.DigitalPin2).ChangeState(GrovePi.Sensors.SensorStatus.Off);
+        }
+    }
+}
